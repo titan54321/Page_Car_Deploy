7067b265aceb83a210f99c2fbbe00b7f111ea3eb
Juan <juan_ls58@hotmail.com>
2025-06-01 22:39:18 -0600

    v0.0.4

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss']
+  templateUrl: './error.component.html'
 })
 export class ErrorComponent {
   @Input() mensaje: string = 'Ha ocurrido un error inesperado.';
