af1a379372d870950ae91d2de170a4b2f28592ff
Juan <juan_ls58@hotmail.com>
2025-06-01 22:36:54 -0600

    v0.0.3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,24 @@
 import { Routes } from '@angular/router';
-import { error } from 'console';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
-    },
-    {
-        path: 'principal',
-        loadComponent: () => import('./principal/principal.component').then(m => m.PrincipalComponent)
-    },
-    {
-        
-        path: '**',
-            loadComponent: () => import('./error/error.component').then(m => m.ErrorComponent),
-    },
-    {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-    }
-
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'principal',
+    loadComponent: () =>
+      import('./principal/principal.component').then(m => m.PrincipalComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./error/error.component').then(m => m.ErrorComponent)
+  }
 ];
